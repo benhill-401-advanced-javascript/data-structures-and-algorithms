5c0e5d24358300260c6bf7f28a1e819a2aa93670
BEN <benjamin.hill28@gmail.com>
2020-10-03 22:58:10 -0700

    began scaffold and code implementation

---
+++ data-structures/binary-tree/model/node.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const Node = module.exports = function (value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+};
---
+++ data-structures/binary-tree/tree.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class BinaryTree {
+  constructor() {
+    this.value = 
+  }
+}
+
+//Create a BinaryTree class
+// Define a method for each of the depth first traversals called preOrder, inOrder, and postOrder which returns an array of the values, ordered appropriately.
