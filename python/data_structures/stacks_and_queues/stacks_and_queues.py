fbe7f157299e1447f1a1d8df85808a9d7d75d933
BEN <benjamin.hill28@gmail.com>
2021-01-12 21:40:43 -0800

    most tests passing for stack complete

--- python/data_structures/stacks_and_queues/stacks_and_queues.py
+++ python/data_structures/stacks_and_queues/stacks_and_queues.py
@@ -1,3 +1,5 @@
+class InvalidOperationError(BaseException):
+  pass
 
 class Node:
   def __init__(self, value, next=None):
@@ -8,24 +10,39 @@ class Stack:
   def __init__(self, top=None):
     self.top = top
 
-  # def top(self):
-  #   pass
-
-  def push(self):
-    pass
-
-  def pop(self):
-    pass
-
-  def peek(self):
-    pass
+  def __str__(self):
+    output = ''
+    current = self.top
+    while current is not None:
+      output += f'{{ {current.value} }} -> '
+      current = current.next
+    return output + 'NONE'
 
   def is_empty(self):
-    pass
+    return True
+  
+  def push(self, value):
+    node = Node(value)
+    node.next = self.top
+    self.top = node
+
+  def pop(self):
+    # if self.is_empty():
+    #   raise InvalidOperationError("Stack is empty!")
+    node = self.top
+    self.top = self.top.next
+    return node.value
+
+  def peek(self):
+    # if self.is_empty():
+    #   raise InvalidOperationError("Stack is empty!")
+    return self.top.value
+
 
 class Queue:
-  def __init__(self, front=None):
-    self.front = front
+  def __init__(self):
+    self.front = None
+    self.rear = None
 
   def enqueue(self):
     pass
@@ -41,8 +58,18 @@ class Queue:
 
 
 if __name__ == "__main__":
-  new_node = Node()
+  new_node = Node(1)
   stack = Stack()
   queue = Queue()
+  print(stack)
+
+  stack.push(2)
+  stack.push(1)
+  stack.push('TOP')
+  print(stack)
+
+  stack.pop()
+  stack.pop()
+  print(stack)
 
 
--- python/tests/test_stacks_and_queues.py
+++ python/tests/test_stacks_and_queues.py
@@ -1,4 +1,4 @@
-from data_structures.stacks_and_queues.stacks_and_queues import Stack, Queue
+from data_structures.stacks_and_queues.stacks_and_queues import Stack, Queue, InvalidOperationError
 
 import pytest
 
@@ -11,16 +11,78 @@ def test_instantiate_queue():
   assert tester.front == None
 
 # Can successfully push onto a stack
+def test_push_onto_stack():
+  stack = Stack()
+  stack.push(1)
+  actual = stack.top.value
+  expected = 1
+  assert actual == expected
+
+def test_push_onto_stack_not_none():
+  stack = Stack()
+  stack.push(1)
+  assert stack.top.value != None
 
 # Can successfully push multiple values onto a stack
+def test_push_multiple():
+  stack = Stack()
+  stack.push(1)
+  stack.push(2)
+  stack.push(3)
+  actual = stack.top.value
+  expected = 3
+  assert actual == expected
+
+def test_push_multiple_fails():
+  stack = Stack()
+  stack.push(1)
+  stack.push(2)
+  stack.push(3)
+  assert stack.top.value != 2
 
 # Can successfully pop off the stack
+def test_pop_one():
+  stack = Stack()
+  stack.push(1)
+  actual = stack.pop()
+  expected = 1
+  assert actual == expected
+
+def test_pop_when_multiple():
+  stack = Stack()
+  stack.push(1)
+  stack.push(2)
+  stack.push(3)
+  stack.pop()
+  stack.pop()
+  actual = stack.pop()
+  expected = 1
+  assert actual == expected
 
 # Can successfully empty a stack after multiple pops
+def test_is_empty():
+  stack = Stack()
+  stack.push(1)
+  stack.pop()
+  actual = stack.is_empty()
+  expected = True
+  assert actual == expected
 
 # Can successfully peek the next item on the stack
+def test_peek():
+  stack = Stack()
+  stack.push(1)
+  actual = stack.peek()
+  expected = 1
+  assert actual == expected
 
 # Can successfully instantiate an empty stack
+def test_raise_exception_if_empty():
+  stack = Stack()
+  with pytest.raises(InvalidOperationError) as e:
+    stack.peek()
+
+  assert str(e.value) == "Stack is empty!"
 
 # Calling pop or peek on empty stack raises exception
 
